01c9e863851abb19c1b94df20b3a713d6a68e817
Erik Bates <erik.s.bates@gmail.com>
2017-04-04 21:02:19 -0500

    Password mismatch error

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
 import webapp2
 
 
-content_header = ""
+#content_header = ""
 
-content = """
-<form method="post" action="/signup">
-    <label>First Name:</label> <input type="text" name="fname"><br>
-    <label>Last Name:</label> <input type="text" name="lname"><br>
-    <label>email:</label> <input type="text" name="email"><br>
-    <label>Password:</label> <input type="password" name="password"><br>
-    <label>Repeat Password:</label> <input type="password" name="password2"><br>
-    <button>Sign Up</button>
-</form>
-"""
+#content = """
+#<form method="post" action="/signup">
+#    <label>First Name:</label> <input type="text" name="fname"><br>
+#    <label>Last Name:</label> <input type="text" name="lname"><br>
+#    <label>email:</label> <input type="text" name="email"><br>
+#    <label>Password:</label> <input type="password" name="password"><br>
+#    <label>Repeat Password:</label> <input type="password" name="password2"><br>
+#    <button>Sign Up</button>
+#</form>
+#"""
 
 username = ""
 email = ""
+uError = ""
+pError = ""
+eError = ""
 
-def build_page(fname,lname,email,password,password2):
+def build_page(username='',uError='',eError='',pError=''):
     username_label = "<label><p>Name: </p></label>"
-    username_input = "<input type='text' name='username' value='" + username + "'/>"
+    username_input = "<input type='text' name='username' value='" + username + "'/> {0}".format(uError)
 
     email_label = "<label><p>EMail: </p></label>"
-    email_input = "<input type='text' name='email' value='" + email + "'/>"
+    email_input = "<input type='text' name='email' value='" + email + "'/> {0}".format(eError)
 
     password_label = "<label><p>Password: </p></label>"
-    password_input = "<input type='text' name='password'>"
+    password_input = "<input type='text' name='password'> {0}".format(pError)
 
     password2_label = "<label><p>Repeat Password: </p></label>"
     password2_input = "<input type='text' name='password2'>"
@@ -42,21 +45,28 @@ def build_page(fname,lname,email,password,password2):
     return form
 
 
-class MainHandler(webapp2.RequestHandler):
+class Index(webapp2.RequestHandler):
     def get(self):
-        self.response.write(build_page("a","b","c","d","e"))
+        self.response.write(build_page('','','',''))
 
-
-class SignUpHandler(webapp2.RequestHandler):
     def post(self):
-        pw = self.request.get("password")
-        pw2 = self.request.get("password2")
+        pw = self.request.get('password')
+        pw2 = self.request.get('password2')
+        username = self.request.get('fname')
+        email = self.request.get('email')
+
         if pw != pw2:
-            error = "Please be sure passwords match"
+            pError = "Please be sure passwords match"
+        else:
+            pError = ""
+
+        content = build_page(username,uError,eError,pError)
+
+        self.response.write(content)
+
 
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/signup', SignUpHandler)
+    ('/', Index)
 ], debug=True)
