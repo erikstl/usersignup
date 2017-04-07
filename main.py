8fd9ca6294164869e3589c4934fb95bea6f45004
Erik Bates <erik.s.bates@gmail.com>
2017-04-06 19:54:30 -0500

    added validation functions

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import webapp2
-
-
-#content_header = ""
-
-#content = """
-#<form method="post" action="/signup">
-#    <label>First Name:</label> <input type="text" name="fname"><br>
-#    <label>Last Name:</label> <input type="text" name="lname"><br>
-#    <label>email:</label> <input type="text" name="email"><br>
-#    <label>Password:</label> <input type="password" name="password"><br>
-#    <label>Repeat Password:</label> <input type="password" name="password2"><br>
-#    <button>Sign Up</button>
-#</form>
-#"""
+import re
 
 username = ""
 email = ""
 uError = ""
 pError = ""
 eError = ""
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+EMAIL_RE = re.compile("^[\S]+@[\S]+.[\S]+$")
+PASS_RE = re.compile("^.{3,20}$")
+
+def valid_username(username):
+    return USER_RE.match(username)
+
+def valid_email(email):
+    return EMAIL_RE.match(email)
+
+def valid_password(password):
+    return PASS_RE.match(password)
 
 def build_page(username='',uError='',eError='',pError=''):
     username_label = "<label><p>Name: </p></label>"
@@ -28,10 +28,10 @@ def build_page(username='',uError='',eError='',pError=''):
     email_input = "<input type='text' name='email' value='" + email + "'/> {0}".format(eError)
 
     password_label = "<label><p>Password: </p></label>"
-    password_input = "<input type='text' name='password'> {0}".format(pError)
+    password_input = "<input type='password' name='password'> {0}".format(pError)
 
     password2_label = "<label><p>Repeat Password: </p></label>"
-    password2_input = "<input type='text' name='password2'>"
+    password2_input = "<input type='password' name='password2'>"
 
     submit_button = "<input type='submit' value='Sign Up'>"
 
@@ -52,13 +52,28 @@ class Index(webapp2.RequestHandler):
     def post(self):
         pw = self.request.get('password')
         pw2 = self.request.get('password2')
-        username = self.request.get('fname')
+        username = self.request.get('username')
         email = self.request.get('email')
+
+        if not valid_username(username):
+            uError = "That is not a valid username"
+        else:
+            uError = ""
 
         if pw != pw2:
             pError = "Please be sure passwords match"
+        elif not valid_password(pw):
+            pError = "Please enter a valid password"
         else:
             pError = ""
+
+        if email == "":
+            eError = ""
+        elif not valid_email(email):
+            eError = "Please enter a valid email address"
+        else:
+            eError = ""
+
 
         content = build_page(username,uError,eError,pError)
 
