8c57b5f4f592385d4d56f63729d4b3fe999b32b4
Erik Bates <erik.s.bates@gmail.com>
2017-04-08 20:15:49 -0500

    Added validation, and welcome redirect.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import webapp2
 import re
 
-username = ""
-email = ""
-uError = ""
-pError = ""
-eError = ""
+#username = ""
+#email = ""
+#uError = ""
+#pError = ""
+#eError = ""
+
 
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 EMAIL_RE = re.compile("^[\S]+@[\S]+.[\S]+$")
@@ -20,18 +21,18 @@ def valid_email(email):
 def valid_password(password):
     return PASS_RE.match(password)
 
-def build_page(username='',uError='',eError='',pError=''):
-    username_label = "<label><p>Name: </p></label>"
-    username_input = "<input type='text' name='username' value='" + username + "'/> {0}".format(uError)
+def build_page(username='',email='',uError='',eError='',pError='',vError=''):
+    username_label = "<label><p>Name: </label>"
+    username_input = "<input type='text' name='username' value='" + username + "'/> {0}</p>".format(uError)
 
-    email_label = "<label><p>EMail: </p></label>"
-    email_input = "<input type='text' name='email' value='" + email + "'/> {0}".format(eError)
+    email_label = "<label><p>EMail: </label>"
+    email_input = "<input type='text' name='email' value='" + email + "'/> {0}</p>".format(eError)
 
-    password_label = "<label><p>Password: </p></label>"
-    password_input = "<input type='password' name='password'> {0}".format(pError)
+    password_label = "<label><p>Password: </label>"
+    password_input = "<input type='password' name='password'> {0}</p>".format(pError)
 
-    password2_label = "<label><p>Repeat Password: </p></label>"
-    password2_input = "<input type='password' name='password2'>"
+    password2_label = "<label><p>Repeat Password: </label>"
+    password2_input = "<input type='password' name='password2'> {0}</p>".format(vError)
 
     submit_button = "<input type='submit' value='Sign Up'>"
 
@@ -47,41 +48,55 @@ def build_page(username='',uError='',eError='',pError=''):
 
 class Index(webapp2.RequestHandler):
     def get(self):
-        self.response.write(build_page('','','',''))
+        self.response.write(build_page('','','','',''))
 
     def post(self):
         pw = self.request.get('password')
         pw2 = self.request.get('password2')
         username = self.request.get('username')
         email = self.request.get('email')
+        error = False
 
         if not valid_username(username):
             uError = "That is not a valid username"
+            error = True
         else:
             uError = ""
 
-        if pw != pw2:
-            pError = "Please be sure passwords match"
-        elif not valid_password(pw):
-            pError = "Please enter a valid password"
+        if not valid_password(pw):
+            pError = "Please enter a valid password."
+            error = True
         else:
             pError = ""
+
+        if pw != pw2:
+            vError = "Please be sure passwords match."
+            error = True
+        else:
+            vError = ""
 
         if email == "":
             eError = ""
         elif not valid_email(email):
             eError = "Please enter a valid email address"
+            error = True
         else:
             eError = ""
 
+        if error:
+            content = build_page(username,email,uError,eError,pError,vError)
+            self.response.write(content)
+        else:
+            self.redirect('/welcome/?username={0}'.format(username))
 
-        content = build_page(username,uError,eError,pError)
 
-        self.response.write(content)
-
-
-
+class Welcome(webapp2.RequestHandler):
+    def get(self):
+        username = self.request.get('username')
+        message = '<h1>Welcome, ' + username + '!</h1>'
+        self.response.write(message)
 
 app = webapp2.WSGIApplication([
-    ('/', Index)
+    ('/', Index),
+    ('/welcome/', Welcome)
 ], debug=True)
